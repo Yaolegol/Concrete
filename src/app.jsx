86867333dcb21795da645cfcb274cf1a6b6782c5
Oleg <m160160@yandex.ru>
2020-10-18 16:22:01 +0700

    add basic routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import {hot} from 'react-hot-loader/root';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 const App = () => {
-    return <div>test</div>;
+    return <Router>
+        <Switch>
+            <Route path="/about">
+                <div>about</div>
+            </Route>
+            <Route path="/users">
+                <div>users</div>
+            </Route>
+            <Route path="/">
+                <div>root page</div>
+            </Route>
+        </Switch>
+    </Router>;
 }
 
 export default hot(App);
