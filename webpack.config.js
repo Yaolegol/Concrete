df44a08b7f54033352c97f5b6d6c42ddb0fdfa37
Oleg <m160160@yandex.ru>
2020-10-25 19:24:40 +0700

    add flow

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
                         loader: 'css-loader',
                         options: {
                             modules: {
-                                localIdentName: '[local]__[hash:base64:5]'
+                                localIdentName: '[local]=[hash:base64:5]'
                             }
                         }
                     },
