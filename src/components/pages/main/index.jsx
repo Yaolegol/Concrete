42996112995cbaf1ae113a0114ece2364d1c597e
Oleg <m160160@yandex.ru>
2020-10-25 20:52:28 +0700

    remove useless files

--- src/components/pages/main/index.jsx
+++ src/components/pages/main/index.jsx
@@ -2,7 +2,6 @@
 import { Layout } from 'common/layout'
 import { Hero } from 'pages/main/hero'
 import React from 'react'
-// import style from './index.module.less'
 
 export const MainPage = () => {
     return (
