b73124373ba18908355225557546f11e4897a838
Oleg <m160160@yandex.ru>
2020-10-26 01:02:17 +0700

    add socials block

--- src/components/common/header/index.jsx
+++ src/components/common/header/index.jsx
@@ -6,6 +6,12 @@ export const Header = (): React$Node => {
     return (
         <div className='header'>
             <img src="src/img/main/header/logo.jpg" alt="Logo"/>
+            <div>
+                <img src="src/img/main/header/social/facebook.png" alt="Facebook"/>
+                <img src="src/img/main/header/social/vk.png" alt="Vk"/>
+                <img src="src/img/main/header/social/instagram.png" alt="Instagram"/>
+                <img src="src/img/main/header/social/youtube.png" alt="Youtube"/>
+            </div>
         </div>
     )
 }
