108003a3425337dc51988acb92e96b5e91cc0181
Oleg <m160160@yandex.ru>
2020-11-01 18:49:18 +0700

    add map

--- src/components/common/Layout/Footer/index.jsx
+++ src/components/common/Layout/Footer/index.jsx
@@ -1,4 +1,5 @@
 // @flow
+import { GoogleMap } from 'common/GoogleMap'
 import React from 'react'
 import './index.less'
 
@@ -6,6 +7,12 @@ export const Footer = (): React$Node => {
     return (
         <div className='footer'>
             <h2>contacts</h2>
+            <div className='footer__content-section'>
+                <div className='footer__map'>
+                    <GoogleMap />
+                </div>
+                <div className='footer__content-container'></div>
+            </div>
         </div>
     )
 }
