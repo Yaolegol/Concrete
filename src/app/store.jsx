e4583be0501bbff1a1f78ae8e1dadbf705b7c92f
Oleg <m160160@yandex.ru>
2020-11-04 16:39:54 +0700

    setup thunk

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,5 +1,9 @@
 // @flow
+import { applyMiddleware, compose, createStore } from 'redux'
+import thunk from 'redux-thunk'
 import { rootReducer } from './reducer'
-import { createStore } from 'redux'
 
-export const store = createStore(rootReducer, {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const middlewares = [thunk]
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+export const store = createStore(rootReducer, {}, compose(applyMiddleware(...middlewares), reduxDevTools))
