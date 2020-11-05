bbafbfab1c34c5d4b6f5f785e1902d3cd8b717af
Oleg <m160160@yandex.ru>
2020-11-05 23:02:32 +0700

    adaptive portfolio section

--- src/modules/pages/Main/Portfolio/index.jsx
+++ src/modules/pages/Main/Portfolio/index.jsx
@@ -10,13 +10,15 @@ export const Portfolio = () => {
             <div className='main-portfolio__image-section'>
                 <div className='main-portfolio__image-container'>
                     <img className='main-portfolio__image-main' src="src/img/main/portfolio/main.jpg" alt="House"/>
-                    <img className='main-portfolio__image-secondary main-portfolio__image-secondary_left-top' src="src/img/main/portfolio/secondary-lt.jpg" alt="Room"/>
-                    <img className='main-portfolio__image-secondary main-portfolio__image-secondary_right-top' src="src/img/main/portfolio/secondary-rt.jpg" alt="Room"/>
-                    <img className='main-portfolio__image-secondary main-portfolio__image-secondary_left-bottom' src="src/img/main/portfolio/secondary-lb.jpg" alt="Room"/>
-                    <img className='main-portfolio__image-secondary main-portfolio__image-secondary_right-bottom' src="src/img/main/portfolio/secondary-rb.jpg" alt="Room"/>
+                    <div className='main-portfolio__secondary-images-container'>
+                        <img className='main-portfolio__image-secondary main-portfolio__image-secondary_left-top' src="src/img/main/portfolio/secondary-lt.jpg" alt="Room"/>
+                        <img className='main-portfolio__image-secondary main-portfolio__image-secondary_right-top' src="src/img/main/portfolio/secondary-rt.jpg" alt="Room"/>
+                        <img className='main-portfolio__image-secondary main-portfolio__image-secondary_left-bottom' src="src/img/main/portfolio/secondary-lb.jpg" alt="Room"/>
+                        <img className='main-portfolio__image-secondary main-portfolio__image-secondary_right-bottom' src="src/img/main/portfolio/secondary-rb.jpg" alt="Room"/>
+                    </div>
                 </div>
             </div>
-            <div>
+            <div className='main-portfolio__button-container'>
                 <Button theme='white'>portfolio</Button>
             </div>
         </div>
