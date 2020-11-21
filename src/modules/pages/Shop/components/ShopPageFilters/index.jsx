cd669a26bd937a8e822def9186a31c140f39a108
Oleg <m160160@yandex.ru>
2020-11-22 00:00:27 +0700

    add price filter

--- src/modules/pages/Shop/components/ShopPageFilters/index.jsx
+++ src/modules/pages/Shop/components/ShopPageFilters/index.jsx
@@ -1,17 +1,32 @@
 // @flow
 import { Range } from 'rc-slider'
-import React from 'react'
+import React, { useState } from 'react'
 import './index.less'
 
 export const ShopPageFilters = () => {
+    const [priceFilterValue, setPriceFilterValue] = useState([0, 1000000])
+
     return (
         <div className='shop-page-filters'>
             <h6>Filters</h6>
             <div className='shop-page-filters__container'>
                 <div>
-                    <p>Price</p>
-                    <div>
-                        <Range />
+                    <h6 className='shop-page-filters__title'>Price</h6>
+                    <div className='shop-page-filters__item'>
+                        <div>
+                            <div>Min {priceFilterValue[0]}</div>
+                            <div>Max {priceFilterValue[1]}</div>
+                        </div>
+                        <div className='shop-page-filters__item'>
+                            <Range
+                                defaultValue={[0, 1000000]}
+                                max={1000000}
+                                onChange={(val) => {
+                                    console.log(val)
+                                    setPriceFilterValue(val)
+                                }}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
