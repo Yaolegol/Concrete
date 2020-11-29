dd2c0ad6a2d6e5a7e386465174448b7491c3cb93
Oleg <m160160@yandex.ru>
2020-11-29 22:44:15 +0700

    fix input

--- src/modules/common/components/Input/index.jsx
+++ src/modules/common/components/Input/index.jsx
@@ -1,17 +1,20 @@
 // @flow
-import cn from "classnames";
 import React from "react";
 import "./index.less";
 
-export const Input = (): React$Node => {
+type TProps = {
+    placeholder: string,
+};
+
+export const Input = ({ placeholder, ...rest }: TProps): React$Node => {
     return (
         <div className="input-container">
             <input
+                {...rest}
                 className="input-container__input"
                 placeholder=" "
-                type="text"
             />
-            <span className="input-container__placeholder">Placeholder</span>
+            <span className="input-container__placeholder">{placeholder}</span>
             <span className="input-container__bottom-line" />
         </div>
     );
