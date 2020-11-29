b30455a7beeeee4b19277ed8a0148588e392a0e2
Oleg <m160160@yandex.ru>
2020-11-29 16:45:29 +0700

    add translate for footer

--- src/modules/common/components/Layout/Footer/index.jsx
+++ src/modules/common/components/Layout/Footer/index.jsx
@@ -1,68 +1,72 @@
 // @flow
-import { TabsAddressContent } from 'common/components/Tabs/TabsAddressContent'
-import { GoogleMap } from 'common/components/GoogleMap'
-import { Tabs } from 'common/components/Tabs'
-import React from 'react'
-import './index.less'
+import { TabsAddressContent } from "common/components/Tabs/TabsAddressContent";
+import { GoogleMap } from "common/components/GoogleMap";
+import { Tabs } from "common/components/Tabs";
+import React from "react";
+import { FormattedMessage } from "react-intl";
+import "./index.less";
 
 const _tabs = [
     {
         id: 1,
-        name: 'test1'
+        name: <FormattedMessage id="footer.tabs.tab1.title" />,
     },
     {
         id: 2,
-        name: 'test2'
+        name: <FormattedMessage id="footer.tabs.tab2.title" />,
     },
     {
         id: 3,
-        name: 'test3'
-    }
-]
+        name: <FormattedMessage id="footer.tabs.tab3.title" />,
+    },
+];
 
 const tabsContent = [
     {
         id: 1,
         data: {
-            address: 'address1',
-            email: 'email1',
-            phone: 'phone1'
-        }
+            address: <FormattedMessage id="common.address.address1" />,
+            email: <FormattedMessage id="common.email.email1" />,
+            phone: <FormattedMessage id="common.phone.phone1" />,
+        },
     },
     {
         id: 2,
         data: {
-            address: 'address2',
-            email: 'email2',
-            phone: 'phone2'
-        }
+            address: <FormattedMessage id="common.address.address2" />,
+            email: <FormattedMessage id="common.email.email2" />,
+            phone: <FormattedMessage id="common.phone.phone2" />,
+        },
     },
     {
         id: 3,
         data: {
-            address: 'address3',
-            email: 'email3',
-            phone: 'phone3'
-        }
-    }
-]
+            address: <FormattedMessage id="common.address.address3" />,
+            email: <FormattedMessage id="common.email.email3" />,
+            phone: <FormattedMessage id="common.phone.phone3" />,
+        },
+    },
+];
 
 export const Footer = (): React$Node => {
     return (
-        <div className='footer'>
-            <h3>contacts</h3>
-            <div className='footer__content-section'>
-                <div className='footer__map-container'>
+        <div className="footer">
+            <h3>
+                <FormattedMessage id="common.contacts.title" />
+            </h3>
+            <div className="footer__content-section">
+                <div className="footer__map-container">
                     <GoogleMap />
                 </div>
-                <div className='footer__content-container'>
+                <div className="footer__content-container">
                     <Tabs
                         activeTabId={1}
                         content={tabsContent}
                         contentComponent={TabsAddressContent}
-                        tabs={_tabs}/>
+                        tabs={_tabs}
+                    />
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
