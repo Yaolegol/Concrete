4a7f032735e7f65dbc5c2ae00ac206fdfaba7db1
Oleg <m160160@yandex.ru>
2020-12-20 02:42:29 +0700

    add get user when load app

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,12 @@
 // @flow
+import { actionGetUser } from "common/actions";
 import { MainPage } from "pages/Main";
 import CartPage from "pages/Cart";
 import LogIn from "pages/LogIn";
 import ShopPage from "pages/Shop";
 import SignUp from "pages/SignUp";
 import { UserPage } from "pages/User/components";
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { hot } from "react-hot-loader/root";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -13,7 +14,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const App = () => {
     const dispatch = useDispatch();
 
-    
+    useEffect(() => {
+        dispatch(actionGetUser());
+    }, [dispatch]);
 
     return (
         <Router>
