0db08a5e7c54963709899ba127f8170ee5f42621
Oleg <m160160@yandex.ru>
2021-01-06 00:40:37 +0700

    add file input

--- src/common/components/FileInput/index.jsx
+++ src/common/components/FileInput/index.jsx
@@ -1,14 +1,49 @@
 // @flow
-import React from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import "./index.less";
 
-export const FileInput = (props: any): React$Node => {
+export const FileInput = (): React$Node => {
+    const [previewUrlList, setPreviewUrlList] = useState([]);
+
+    const handleChange = useCallback((e) => {
+        const urls = [];
+        [...e.target.files].forEach((file) => {
+            const { name } = file;
+            const url = URL.createObjectURL(file);
+            urls.push({ name, url });
+        });
+        setPreviewUrlList(urls);
+    }, []);
+
+    const preview = useMemo(() => {
+        return previewUrlList.map(({ name, url }) => {
+            return (
+                <div className="file-input__preview-card" key={url}>
+                    <img
+                        alt={name}
+                        className="file-input__preview-img"
+                        src={url}
+                    />
+                    <div className="file-input__preview-description">
+                        {name}
+                    </div>
+                </div>
+            );
+        });
+    }, [previewUrlList]);
+
     return (
         <div className="file-input">
             <label className="file-input__label">
-                <input className="file-input__input" multiple type="file" />
+                <input
+                    className="file-input__input"
+                    multiple
+                    onChange={handleChange}
+                    type="file"
+                />
                 <span>Выберите файлы</span>
             </label>
+            <div className="file-input__preview-section">{preview}</div>
         </div>
     );
 };
