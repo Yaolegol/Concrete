c865e08b87455d29f46a875a62f3e0109a36d1ca
Oleg <m160160@yandex.ru>
2021-02-07 22:59:21 +0700

    fix dev server

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,4 +1,5 @@
 const customRequire = require('app-root-path').require;
+const path = require('path');
 const adminCreateProductRoute = customRequire('server/routes/admin/createProduct');
 const adminGetPurchasesRoute = customRequire('server/routes/admin/getPurchases');
 const loginRoute = customRequire('server/routes/login');
@@ -21,8 +22,11 @@ router
 
 if (process.env.NODE_ENV === 'production') {
     router.get('/*', (req, res) => {
-        const path = require('path')
         res.sendFile(path.resolve('/app', 'dist', 'index.html'))
+    })
+} else {
+    router.get('/*', (req, res) => {
+        res.sendFile(path.resolve('dist', 'index.html'))
     })
 }
 
