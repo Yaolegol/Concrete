8784a19a17e745583566313e841e6aeafd44b2c3
Oleg <m160160@yandex.ru>
2023-01-04 02:07:47 +0700

    refactor footer

--- src/modules/Layout/Footer/index.jsx
+++ src/modules/Layout/Footer/index.jsx
@@ -51,7 +51,7 @@ const tabsContent = [
 export const Footer = (): React$Node => {
     return (
         <div className="footer">
-            <h3>
+            <h3 className="footer__title">
                 <FormattedMessage id="common.contacts.title" />
             </h3>
             <div className="footer__content-section">
