b3c55a31d1a6cec2691f583dc83be4e2bd5fadcb
Oleg <m160160@yandex.ru>
2023-01-04 19:34:35 +0700

    fix description + portfolio sections

--- src/main/Home/Description/index.jsx
+++ src/main/Home/Description/index.jsx
@@ -8,18 +8,16 @@ export const Description = (): React$Node => {
     return (
         <div className="main-home-description">
             <div className="main-home-description__content-section">
-                <div className="main-home-description__content">
-                    <h3 className="main-home-description__content-title">
-                        <FormattedMessage id="main.descriptionSection.title" />
-                    </h3>
-                    <p className="main-home-description__content-description">
-                        <FormattedMessage id="main.descriptionSection.description" />
-                    </p>
-                    <div className="main-home-description__about-link-container">
-                        <CustomLink url="#">
-                            <FormattedMessage id="common.learnMore.title" />
-                        </CustomLink>
-                    </div>
+                <h3 className="main-home-description__content-title">
+                    <FormattedMessage id="main.descriptionSection.title" />
+                </h3>
+                <p className="main-home-description__content-description">
+                    <FormattedMessage id="main.descriptionSection.description" />
+                </p>
+                <div className="main-home-description__about-link-container">
+                    <CustomLink url="#">
+                        <FormattedMessage id="common.learnMore.title" />
+                    </CustomLink>
                 </div>
             </div>
             <div className="main-home-description__image-section">
--- src/main/Home/Portfolio/index.jsx
+++ src/main/Home/Portfolio/index.jsx
@@ -11,34 +11,32 @@ export const Portfolio = (): React$Node => {
                 <FormattedMessage id="common.portfolio.title" />
             </h3>
             <div className="main-portfolio__image-section">
-                <div className="main-portfolio__image-container">
+                <img
+                    className="main-portfolio__image-main"
+                    src="/images/main/portfolio/main.jpg"
+                    alt="House"
+                />
+                <div className="main-portfolio__secondary-images-container">
                     <img
-                        className="main-portfolio__image-main"
-                        src="/images/main/portfolio/main.jpg"
-                        alt="House"
+                        className="main-portfolio__image-secondary main-portfolio__image-secondary_left-top"
+                        src="/images/main/portfolio/secondary-lt.jpg"
+                        alt="Room"
                     />
-                    <div className="main-portfolio__secondary-images-container">
-                        <img
-                            className="main-portfolio__image-secondary main-portfolio__image-secondary_left-top"
-                            src="/images/main/portfolio/secondary-lt.jpg"
-                            alt="Room"
-                        />
-                        <img
-                            className="main-portfolio__image-secondary main-portfolio__image-secondary_right-top"
-                            src="/images/main/portfolio/secondary-rt.jpg"
-                            alt="Room"
-                        />
-                        <img
-                            className="main-portfolio__image-secondary main-portfolio__image-secondary_left-bottom"
-                            src="/images/main/portfolio/secondary-lb.jpg"
-                            alt="Room"
-                        />
-                        <img
-                            className="main-portfolio__image-secondary main-portfolio__image-secondary_right-bottom"
-                            src="/images/main/portfolio/secondary-rb.jpg"
-                            alt="Room"
-                        />
-                    </div>
+                    <img
+                        className="main-portfolio__image-secondary main-portfolio__image-secondary_right-top"
+                        src="/images/main/portfolio/secondary-rt.jpg"
+                        alt="Room"
+                    />
+                    <img
+                        className="main-portfolio__image-secondary main-portfolio__image-secondary_left-bottom"
+                        src="/images/main/portfolio/secondary-lb.jpg"
+                        alt="Room"
+                    />
+                    <img
+                        className="main-portfolio__image-secondary main-portfolio__image-secondary_right-bottom"
+                        src="/images/main/portfolio/secondary-rb.jpg"
+                        alt="Room"
+                    />
                 </div>
             </div>
             <div className="main-portfolio__button-container">
